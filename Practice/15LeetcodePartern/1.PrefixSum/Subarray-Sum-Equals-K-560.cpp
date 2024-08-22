57633f18dbf4952815f31973f43774c860585ae0
HauTranCong <haunek22@gmail.com>
2024-08-22 22:53:44 +0700

    Using hashmap to track frequency of prefix sums

--- Practice/15LeetcodePartern/1.PrefixSum/Subarray-Sum-Equals-K-560.cpp
+++ Practice/15LeetcodePartern/1.PrefixSum/Subarray-Sum-Equals-K-560.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int total = 0;
-        int prefixSum = 0;
-        for(int i = 0; i < nums.size(); ++i) {
-            for(int j = i; j < nums.size(); ++j) { 
-                prefixSum += nums[j];
-                total += prefixSum == k ? 1 : 0;
-                // cout << prefixSum << "\n";
-            }
-            prefixSum = 0;
-        }
+        int ans = 0;
+        int prefix = 0;
+        unordered_map<int, int> m{{0,1}};
 
-        return total;
+        for(const int num : nums) {
+            prefix += num;       
+            int target = prefix - k; 
+            auto it = m.find(target);
+            if(it != m.end())
+                ans += it->second;
+            m[prefix]++;
+        }
     }
 };
+
+
+// Brute force but 92/93 with TLE
+// int total = 0;
+// int prefixSum = 0;
+// for(int i = 0; i < nums.size(); ++i) {
+//     for(int j = i; j < nums.size(); ++j) { 
+//         prefixSum += nums[j];
+//         total += prefixSum == k ? 1 : 0;
+//     }
+//     prefixSum = 0;
+// }
+// return total;
