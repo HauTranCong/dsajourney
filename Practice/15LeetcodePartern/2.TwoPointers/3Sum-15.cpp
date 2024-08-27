d3a920d6f35834ccb84e1e0f617589d592690546
HauTranCong <haunek22@gmail.com>
2024-08-27 17:32:46 +0700

    15, Runtime 22.6%, Memory 20.35%

--- Practice/15LeetcodePartern/2.TwoPointers/3Sum-15.cpp
+++ Practice/15LeetcodePartern/2.TwoPointers/3Sum-15.cpp
@@ -5,24 +5,30 @@ public:
         if(count(nums.begin(), nums.end(), 0) == nums.size())
             return {{0,0,0}};
         vector<vector<int>> ans;
-        int i = 0;
-        int j = i + 1;
-        int k = nums.size() - 1;
-        while(j < k) {
-            int sum = nums[i] + nums[j] + nums[k];
-            if(sum == 0) {
-                ans.push_back({nums[i],nums[j],nums[k]});
-                --k;
-            } else if(sum > 0) {
-                --k;
-            } else {
-                ++i;
-                j = i + 1;
-            }
+        map<vector<int>, int> m;
+        for(int i = 0; i < nums.size(); i++) {
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0) {
+                    m[{nums[i],nums[j],nums[k]}] = 1;
+                    --k;
+                } else if(sum > 0) {
+                    --k;
+                } else {
+                    ++j;
+                }
+            }            
         }
+
+        for(const auto& it : m)
+            ans.push_back(it.first);
+            
         return ans; 
     }
 };
+// be careful with 0, 0, 0, 0 cases
 
 //  -4  -1  -1  0   1   2
 //  i   j               k
@@ -30,4 +36,6 @@ public:
 //      i   j       k
 //          i   j   k       -> take it  
 
-// be careful with 0, 0, 0, 0 cases
+//  -2  0   1   1   2
+//  i   j           k   -> take it
+//  i   j       k       
