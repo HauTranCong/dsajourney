86c48b7a82e41fa1c7ec4334277dc5d605771f86
HauTranCong <haunek22@gmail.com>
2024-09-02 16:43:06 +0700

    287 - Slow and fast pointers, Runtime 81.88%, Memory 54.38%

--- Practice/15LeetcodePartern/4.FastNSlowPointer/FindtheDuplicateNumber-287.cpp
+++ Practice/15LeetcodePartern/4.FastNSlowPointer/FindtheDuplicateNumber-287.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        map<int,int> m;
-        for(int i = 0; i < nums.size(); ++i){
-            if(++m[nums[i]] > 1)
-                return nums[i];
+        int slow = 0;
+        int fast = 0;
+        while(true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast)
+                break;
         }
-        return 0;
+        slow = 0;
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 };
+
+// 287 - Hashmap to count appearance times, Runtime 6.98%, Memory 10.12%
+// int findDuplicate(vector<int>& nums) {
+//     map<int,int> m;
+//     for(int i = 0; i < nums.size(); ++i){
+//         if(++m[nums[i]] > 1)
+//             return nums[i];
+//     }
+//     return 0;
+// }
